f780e93727753c42ee088b3c84add0af43cd01ca
maaru-jp <33389000+maaru-jp@users.noreply.github.com>
2026-02-03 22:01:30 +0800

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,13 +1,3 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-function addToCart(id){
-  const i = cart.find(x=>x.id===id);
-  if(i) i.qty++; else cart.push({id,qty:1});
-  localStorage.setItem("cart",JSON.stringify(cart));
-}
-
-function checkout(){
-  let t="🛒 MAARU 訂單送出\n";
-  cart.forEach(c=> t+=`商品ID ${c.id} x${c.qty}\n`);
-  location.href="https://line.me/R/oaMessage/@YOUR_LINE_ID/?text="+encodeURIComponent(t);
-}
+let cart=JSON.parse(localStorage.getItem("cart"))||[];
+function addToCart(id){const i=cart.find(x=>x.id===id);if(i)i.qty++;else cart.push({id,qty:1});localStorage.setItem("cart",JSON.stringify(cart));}
+function checkout(){let t="🛒 MAARU 訂單送出\n";cart.forEach(c=>t+=`商品 ${c.id} x${c.qty}\n`);location.href="https://line.me/R/oaMessage/@YOUR_LINE_ID/?text="+encodeURIComponent(t);}
--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// 🔥 Firebase config (replace with your own)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
