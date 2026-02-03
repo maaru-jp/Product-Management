5f776e18dad535d97c448c30fef8bf50f70a5cf2
maaru-jp <33389000+maaru-jp@users.noreply.github.com>
2026-02-03 20:09:12 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+  {
+    id: "SKB-021",
+    name: "三麗鷗芭蕾舞吊飾",
+    price: 690,
+    image: "https://via.placeholder.com/300",
+    description: "日本直送・預購商品",
+    active: true
+  },
+  {
+    id: "SKB-022",
+    name: "三麗鷗泡湯放鬆娃",
+    price: 550,
+    image: "https://via.placeholder.com/300",
+    description: "現貨供應",
+    active: true
+  }
+];
